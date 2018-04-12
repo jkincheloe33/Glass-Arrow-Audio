1a9b1d484d49e11f4398d3bfca8dadce906528d6
Josh Kincheloe <jkincheloe33@gmail.com>
2018-04-12 14:27:15 -0500

    Change the width of the Header container.

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -29,7 +29,7 @@ class Navbar extends Component {
             <nav ref={(nav) => { this.nav = nav; }} className="animated slideInDown nav-container transition navbar navbar-expand-md navbar-light fixed-top">
                 <a className="navbar-brand" href="/">
                     <img src="https://cdn.iconscout.com/public/images/icon/premium/png-512/mice-voice-recording-recorder-3108fd28c9671232-512x512.png" width="30" height="30" className="d-inline-block align-top" alt="" />
-                    Glass Arrow
+                    Glass Arrow Audio
                     </a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
