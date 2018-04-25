e3533fac18206855c4997076e669a638cdf36ad9
Josh Kincheloe <jkincheloe33@gmail.com>
2018-04-25 09:24:36 -0500

    Added npm module for Google maps. Wrote a component that is wrapped by GoogleApiWrapper and includes apiKey.

---
+++ client/src/components/contact.jsx
@@ -0,0 +1,25 @@
+import React, { Component, Fragment } from 'react';
+
+class Contact extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+        return (
+
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-7" id="map">
+
+                    </div>
+                    <div className="col-md-5">
+
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+}
---
+++ client/src/components/mapContainer.jsx
@@ -0,0 +1,9 @@
+import { GoogleApiWrapper } from 'google-maps-react';
+
+// ...
+
+export class MapContainer extends React.Component { }
+
+export default GoogleApiWrapper({
+    apiKey: (AIzaSyBO - ZAjpSbRZHTsM64l1S84qmA0JcmByRw)
+})(MapContainer)
