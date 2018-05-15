6b909288391c068467ea5b553694de859723cc65
Josh Kincheloe <jkincheloe33@gmail.com>
2018-05-15 15:22:03 -0500

    Fixing spacing issue on text.

--- client/src/components/about.jsx
+++ client/src/components/about.jsx
@@ -8,7 +8,7 @@ const About = () => {
                     We are a full service, professional recording studio located in Toledo, Ohio.
                 </h1>
                 <p className="">
-                    We offer everything from basic demo recording services to detailed full band recordings. At GAA our passion is music and it shows in everything we do.Whether you’re recording a full album or just dropping some tracks off to be mixed, we'll take your project on as our own.
+                    We offer everything from basic demo recording services to detailed full band recordings. At GAA our passion is music and it shows in everything we do. Whether you’re recording a full album or just dropping some tracks off to be mixed, we'll take your project on as our own.
                 </p>
             </div>
         </div>
